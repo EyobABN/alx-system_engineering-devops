878a7f6d8b007e75809780c15c3d65f63f7ea8ff
EyobABN <eyob.alamerew.belay@gmail.com>
2022-10-17 16:42:51 +0300

    Remove print commands

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,4 +16,23 @@ if __name__ == "__main__":
     '''
         Execute if not imported as a module
     '''
-    pass
+    if len(sys.argv) > 1:
+        ID = sys.argv[1]
+        x = requests.get(
+                API_URL + '/users/' + ID + '/todos'
+        )
+        todos = json.loads(x.text)
+        done = []
+        numberOfTasks = 0
+        numberOfDone = 0
+        employee = json.loads(
+                requests.get(
+                    API_URL + '/users/' + ID
+                ).text
+        )
+        nameOfEmployee = employee.get('name')
+        for todo in todos:
+            numberOfTasks += 1
+            if todo.get('completed') is True:
+                numberOfDone += 1
+                done.append('\t ' + todo.get('title'))
